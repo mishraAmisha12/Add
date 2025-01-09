81749407f5facb71e8ee91d02755ffab4b0561d8
mishraAmisha12 <mishraamisha1712@gmail.com>
2025-01-09 12:38:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,25 @@
 import java.util.Scanner;
 
-public class AdditionProgram {
+public class MultiplicationProgram {
 
     public static void main(String[] args) {
-      
+        // Create a Scanner object to read input from the user
         Scanner scanner = new Scanner(System.in);
 
-      
+        // Prompt the user for two numbers
         System.out.print("Enter the first number: ");
         int num1 = scanner.nextInt();
 
         System.out.print("Enter the second number: ");
         int num2 = scanner.nextInt();
 
-      
-        int sum = num1 + num2;
+        // Multiply the two numbers
+        int product = num1 * num2;
 
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        // Display the result
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
 
-       
+        // Close the scanner to avoid resource leaks
         scanner.close();
     }
 }
